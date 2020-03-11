b90fc23e8e57f5d0ff3ad00a93b1194c025695d9
Thiago Maia da Costa <thiago.maia971@hotmail.com>
2020-03-10 23:06:57 -0300

    adaline rodando

--- adaline.py
+++ adaline.py
@@ -4,7 +4,7 @@ import math as math
 
 class Adaline():
     
-    def __init__(self, input_size, act_func=heaviside_step_function, epochs=1000, learning_rate=0.0025, precision=0.000001):
+    def __init__(self, input_size, act_func=heaviside_step_function, epochs=1000, learning_rate=0.0025, precision=0.00001):
         self.act_func = act_func
         self.epochs = epochs
         self.learning_rate = learning_rate
@@ -13,41 +13,33 @@ class Adaline():
         
     
     def predict(self, inputs):
-        u = np.dot(inputs, self.weights)
-        return u
-        # return self.act_func(u)
-        
-    # n = learning_rate
-    # y = output model
-    # o = desired output
-    # w = w + n (o-y)x
-    # E = (o-y)²
-    
-    # x^K = conjuto de amostra = training_inputs
-    # d^K = saída desejada = 
-    def train(self, training_inputs, labels):
-        error = True
-        epochs = 0
+        predict = np.dot(inputs, self.weights)
+        return predict
 
-        eqm = 0
-        eqmAnterior = 0
-        eqmAtual = 0
-        while(error == True or epochs < self.epochs):
-            eqmAnterior = eqm/len(training_inputs)
-            for inputs, label in zip(training_inputs, labels):
+    def train(self, training_inputs, labels):
+        epochs = 0.0
+
+        eqmAnterior = 0.0
+        eqmAtual = 0.0
+        while(True):
+            eqmAnterior = eqmAtual
+            eqmAtual = 0.0
+            for inputs, output in zip(training_inputs, labels):
                 inputs = np.append(-1, inputs)
-                u = self.predict(inputs)
-                self.weights = self.weights + self.learning_rate* label - u * inputs
-                eqm += self.EQM(label, u)
+                predict = self.predict(inputs)
+                self.weights += self.learning_rate * (output - predict) * inputs
+                eqmAtual += float(self.EQM(output, predict))
 
             epochs = epochs + 1
-            eqmAtual = eqm
-            if (eqmAnterior - eqmAtual <= self.precision):
-                error = False
-                
-            print(f"epochs: {epochs}")
-            print(f"eqmAnterior: {eqmAnterior}")
-            print(f"eqmAtual: {eqmAtual}")
+            eqmAtual = float(eqmAtual/len(training_inputs))
             
-    def EQM(self, label, u): 
-        return math.pow(label-u, 2)
+            if (epochs > self.epochs or float(abs(eqmAtual-eqmAnterior)) <= self.precision):
+                break
+
+        print(f"epochs: {epochs}")
+        print(f"eqmAnterior: {eqmAnterior}")
+        print(f"eqmAtual: {eqmAtual}")
+        print(f"diff: {abs(eqmAtual - eqmAnterior)}")
+            
+    def EQM(self, output, predict): 
+        return math.pow(output-predict, 2)
